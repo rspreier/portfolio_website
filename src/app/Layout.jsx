a8e15490c69b564d741167bc0af15197142bcb6c
Ryan Spreier <rspreier@gmail.com>
2024-05-15 15:16:57 -0700

    Changed favicon

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function Layout(props) {
     return (
         <>
+            <Head>
+                <title>Ryan Spreier's Website</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Navbar />
             {props.children}
         </>
